36e0e580d035221efebc3a6ae4c2f11de9ba37fc
ChoiYeonho0903 <903yh@naver.com>
2022-01-12 17:41:40 +0900

    Thymeleaf

--- 코배스_웹프로젝트/ex1/ex1/src/main/java/org/zerock/ex1/controller/SampleController.java
+++ 코배스_웹프로젝트/ex1/ex1/src/main/java/org/zerock/ex1/controller/SampleController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.ex1.dto.SampleDTO;
 
 import java.time.LocalDateTime;
@@ -28,7 +29,7 @@ public class SampleController {
         log.info("e");
     }
 
-    @GetMapping("/ex2")
+    @GetMapping({"/ex2", "/exLink"})
     public void exModel(Model model) {
         List<SampleDTO> list = IntStream.rangeClosed(1, 20).asLongStream().mapToObj(i -> {
             SampleDTO dto = new SampleDTO(i, "First_" + i, "Last_" + i, LocalDateTime.now());
@@ -36,4 +37,26 @@ public class SampleController {
         }).collect(Collectors.toList());
         model.addAttribute("list", list);
     }
+
+    @GetMapping({"/exInline"})
+    public String exInline(RedirectAttributes redirectAttributes) {
+        log.info("exInline.......");
+
+        SampleDTO dto = new SampleDTO(100L, "First_100", "Last_100", LocalDateTime.now());
+        redirectAttributes.addFlashAttribute("result", "success");
+        redirectAttributes.addFlashAttribute("dto", dto);
+
+        return "redirect:/sample/ex3";
+    }
+
+    @GetMapping("/ex3")
+    public void ex3() {
+        log.info("ex3");
+    }
+
+    @GetMapping({"/exLayout1", "/exSidebar"})
+    public void exLayout1() {
+        log.info("exLayout.......");
+    }
+
 }
